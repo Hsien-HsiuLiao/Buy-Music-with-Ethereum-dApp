0468de53e706a9aa6fe451042dc6869e87e0be80
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2019-01-07 20:29:42 -0800

    Update TestBandApp.js

--- test/TestBandApp.js
+++ test/TestBandApp.js
@@ -1,1 +1,39 @@
+const BandApp = artifacts.require("BandApp");
+
+//test addMgr function
+//get value of bandmgrindex
+//call addmgr function
+// bandmgrindex should be increasd by 1
+contract("BandApp", accounts => {
+    it("should increase bandmgrindex by 1 when calling addMgr function", () => {
+        let indexBefore;
+        let indexAfter;
+        let contract;
+        return BandApp.deployed()
+        .then(instance => {
+            contract = instance;
+            return instance.bandMgrIndex();
+        })
+        .then(result => {
+            indexBefore = result.toNumber();    
+        })
+        .then( () => {
+            contract.addMgr(accounts[0]);
+        })
+        .then( () => {
+            return contract.bandMgrIndex();
+        })
+        .then(newIndex => {
+            indexAfter = newIndex.toNumber();        
+        })
+        .then( () => {
+            assert.equal(indexBefore+1, indexAfter, "bandMgrIndex should have increased by 1")    
+        })
+    })
+})
+
+//test addSong function
+
+//test buySong function
+
 
